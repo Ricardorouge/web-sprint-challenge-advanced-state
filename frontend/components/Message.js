201e90fb9c2783420255336a2ab392a401e791f0
Ricardorouge <ricky032929@gmail.com>
2022-04-22 12:29:08 -0400

    finished wheel component

--- frontend/components/Message.js
+++ frontend/components/Message.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import * as actionCreators from '../state/action-creators'
 
-export default function Message(props) {
+function Message(props) {
   return <div id="message">Nice job!</div>
 }
+
+
+export default connect(st => st, actionCreators)(Message)
